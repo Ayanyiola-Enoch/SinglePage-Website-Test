82469d7e8437bafbf3fdf988b7549cda31d7aef2
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-06-16 21:17:13 -0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    
+    if(name && email && message) {
+        alert('Thank you for your message!');
+        // Here you can add code to send the form data to your email or server
+    } else {
+        alert('Please fill out all fields.');
+    }
+});
